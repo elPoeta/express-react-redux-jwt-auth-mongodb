3c86982099b3d361ee0ee3d5fa02e4a7a48dbb08
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-02-08 17:49:34 -0300

    setup client side

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                TESTING...
+            </div>
+        )
+    }
+}
+
+export default App;
